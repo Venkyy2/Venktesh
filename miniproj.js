be40a22eebe6462fca3b304fa448ad578cca0126
Venkyy2 <venkyadav2601@gmail.com>
2026-05-20 16:05:10 +0530

    Recaptcha Added

--- miniproj.js
+++ miniproj.js
@@ -1,8 +1,27 @@
-function beforeSubmit(){
-  let outputdate =   document.querySelector(".outputdate");
+let captchachecked = false;
+function captchasuccess() {
+captchachecked = true;
+}
+function beforeSubmit(event){
+  if (captchachecked){
+    let outputdate =   document.querySelector(".outputdate");
   let inputdate =   document.querySelector(".inputdate");
   console.log (inputdate.value);
   let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
   outputdate.value = formattedDate;
 console.log(outputdate);
+  }else{
+    alert("Please complete the CAPTCHA before submitting the form.");
+    event.preventDefault();
+  }
+  
 }
+
+function timestamp() {
+   var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+      var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+      elems["ts"] = JSON.stringify(new Date().getTime());
+      document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } }
+    setInterval(timestamp, 500); 
